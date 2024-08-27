8638255f0500c84c638b6b45c53436a092d97925
ZTZ <tzamirov@gmail.com>
2024-08-27 23:19:06 +0600

    create product details section

--- src/components/sections/product-details.tsx
+++ src/components/sections/product-details.tsx
@@ -1,5 +1,18 @@
 import React from "react"
+import Image from "next/image"
+import { getNormalizedSectionById } from "@/server/data-access-layer/content"
 
-export default function ProductDetails() {
-  return <div>ProductDetails</div>
+export default async function ProductDetails() {
+  const sectionData = await getNormalizedSectionById("product-details")
+  return (
+    <section className="relative h-screen">
+      <Image
+        src={sectionData?.image || "/assets/placeholder-gray.svg"}
+        alt="Product Details Background Image"
+        fill
+        className="object-cover"
+        sizes="(max-width: 768px) 100vw, 50vw"
+      />
+    </section>
+  )
 }
