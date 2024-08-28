f1ff0e948c1830bbd3e5c1c5cd3420f5d265675e
ZTZ <tzamirov@gmail.com>
2024-08-28 18:25:36 +0600

    create q&a + submit request sections

--- src/components/forms/form-request.tsx
+++ src/components/forms/form-request.tsx
@@ -4,21 +4,15 @@ import { zodResolver } from "@hookform/resolvers/zod"
 import { useTranslations } from "next-intl"
 import { useAction } from "next-safe-action/hooks"
 import { useForm } from "react-hook-form"
+import PhoneInput from "react-phone-number-input"
+import "react-phone-number-input/style.css"
 import { toast } from "sonner"
 import { cn } from "@/lib/utils"
 import { Button } from "@/components/ui/button"
-import { Checkbox } from "@/components/ui/checkbox"
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-} from "@/components/ui/form"
+import { Form, FormControl, FormField, FormItem } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { Textarea } from "@/components/ui/textarea"
 import { createClientRequest } from "@/server/actions/request-action"
-import type { NormalizedCard } from "@/types/content.schema"
 import {
   clientRequestCreateSchema,
   type zClientRequestСreate,
@@ -26,20 +20,23 @@ import {
 
 type Props = {
   btnText: string | undefined | null
-  card: NormalizedCard | undefined
+  privacyText: string | undefined | null
   className?: string
 }
 
-export default function FormRequest({ btnText, card, className }: Props) {
+export default function FormRequest({
+  btnText,
+  privacyText,
+  className,
+}: Props) {
   const t = useTranslations("Components.FormRequest")
   const form = useForm<zClientRequestСreate>({
     resolver: zodResolver(clientRequestCreateSchema),
     defaultValues: {
-      firstName: "",
-      lastName: "",
+      fullName: "",
+      phone: "",
       email: "",
       message: "",
-      accepted_privacy_policy: false,
     },
   })
 
@@ -56,7 +53,6 @@ export default function FormRequest({ btnText, card, className }: Props) {
   })
 
   function onSubmit(values: zClientRequestСreate) {
-    // toast(JSON.stringify(values))
     execute(values)
   }
 
@@ -64,57 +60,62 @@ export default function FormRequest({ btnText, card, className }: Props) {
     <Form {...form}>
       <form
         onSubmit={form.handleSubmit(onSubmit)}
-        className={cn(
-          "space-y-5 rounded-[32px] bg-gray-200 px-4 py-10 text-blue-950 shadow-lg md:px-10 lg:px-20 xl:px-36",
-          className,
-        )}
+        className={cn("space-y-5", className)}
       >
-        <div className="grid gap-5 lg:grid-cols-2 lg:gap-8">
-          <div className="space-y-5 lg:space-y-8">
-            <div className="grid gap-5 lg:grid-cols-2">
-              {/* First Name */}
-              <FormField
-                control={form.control}
-                name="firstName"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>{t("name-label")}</FormLabel>
-                    <FormControl>
-                      <Input placeholder={t("name-placeholder")} {...field} />
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
+        <div className="space-y-3">
+          {/* Full Name */}
+          <FormField
+            control={form.control}
+            name="fullName"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    placeholder={t("placeholder-name")}
+                    className="placeholder:text-text-secondary h-auto px-4 py-3 text-base leading-6"
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
 
-              {/* Last Name */}
-              <FormField
-                control={form.control}
-                name="lastName"
-                render={({ field }) => (
-                  <FormItem>
-                    <FormLabel>{t("phone-label")}</FormLabel>
-                    <FormControl>
-                      <Input placeholder="Азаматов" {...field} />
-                    </FormControl>
-                  </FormItem>
-                )}
-              />
-            </div>
+          {/* Phone */}
+          <FormField
+            control={form.control}
+            name="phone"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <PhoneInput
+                    defaultCountry="KG"
+                    international
+                    countryCallingCodeEditable={false}
+                    placeholder={t("placeholder-phone")}
+                    className="placeholder:text-text-secondary h-auto space-x-2 rounded-md border px-4 py-3 text-base leading-6"
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
 
-            {/* Email */}
-            <FormField
-              control={form.control}
-              name="email"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>{t("label-email")}</FormLabel>
-                  <FormControl>
-                    <Input placeholder="azamatov@gmail.com" {...field} />
-                  </FormControl>
-                </FormItem>
-              )}
-            />
-          </div>
+          {/* Email */}
+          <FormField
+            control={form.control}
+            name="email"
+            render={({ field }) => (
+              <FormItem>
+                <FormControl>
+                  <Input
+                    placeholder={t("placeholder-email")}
+                    className="placeholder:text-text-secondary h-auto px-4 py-3 text-base leading-6"
+                    {...field}
+                  />
+                </FormControl>
+              </FormItem>
+            )}
+          />
 
           {/* Message */}
           <FormField
@@ -122,51 +123,27 @@ export default function FormRequest({ btnText, card, className }: Props) {
             name="message"
             render={({ field }) => (
               <FormItem>
-                <FormLabel className="text-sm">
-                  <p className="mb-2">{card?.title}</p>
-                  <p>{card?.description}</p>
-                  <p>{card?.extra}</p>
-                </FormLabel>
-                <ul className="list-inside list-decimal text-sm text-[#475467]">
-                  {card?.bullets.map((bullet, index) => (
-                    <li key={index}>{bullet}</li>
-                  ))}
-                </ul>
                 <FormControl>
                   <Textarea
-                    placeholder="Ваше сообщение..."
-                    className="resize-none"
+                    placeholder={t("placeholder-message")}
+                    className="placeholder:text-text-secondary resize-none text-base leading-6"
                     {...field}
                   />
                 </FormControl>
               </FormItem>
             )}
           />
-        </div>
 
-        {/* Privacy Policy */}
-        <FormField
-          control={form.control}
-          name="accepted_privacy_policy"
-          render={({ field }) => (
-            <FormItem className="flex flex-row items-center gap-3 space-y-0 p-1">
-              <FormControl>
-                <Checkbox
-                  checked={field.value}
-                  onCheckedChange={field.onChange}
-                />
-              </FormControl>
-              <FormLabel>{t("label-privacy-policy")}</FormLabel>
-            </FormItem>
-          )}
-        />
+          {/* Privacy Policy */}
+          <p className="text-text-secondary text-sm">{privacyText}</p>
+        </div>
 
         {/* Submit */}
         <Button
           type="submit"
           variant="core"
           size="lg"
-          className="h-11 w-full"
+          className="text-text-primary w-full p-3 font-semibold leading-6"
           disabled={isExecuting}
         >
           {btnText || t("button-send-request")}
--- src/components/sections/cta.tsx
+++ src/components/sections/cta.tsx
@@ -1,6 +1,5 @@
 import React from "react"
 import Image from "next/image"
-import { getTranslations } from "next-intl/server"
 import FormRequest from "@/components/forms/form-request"
 import Heading from "@/components/shared/heading"
 import Subheading from "@/components/shared/subheading"
@@ -8,25 +7,31 @@ import { getNormalizedSectionById } from "@/server/data-access-layer/content"
 
 export default async function CTA() {
   const sectionData = await getNormalizedSectionById("cta")
-  const card = sectionData?.cards[0]
   return (
-    <div id="cta" className="container mb-24 space-y-8">
-      <div className="flex flex-col gap-3 lg:flex-row lg:justify-between">
+    <div id="cta" className="container mb-20 grid justify-items-center gap-y-8">
+      <div className="space-y-8 rounded-3xl bg-white px-4 py-8">
         <div className="space-y-3">
-          <Heading>{sectionData?.heading}</Heading>
-          <Subheading>{sectionData?.subheading}</Subheading>
+          <Heading className="text-2xl leading-6">
+            {sectionData?.heading}
+          </Heading>
+          <Subheading className="leading-6">
+            {sectionData?.subheading}
+          </Subheading>
         </div>
-        <div className="relative h-[103px] w-[208px] justify-self-center">
-          <Image
-            src={sectionData?.image || "/assets/placeholder-gray.svg"}
-            alt="Company Image"
-            fill
-            className="object-cover"
-            sizes="(max-width: 768px) 100vw, 50vw"
-          />
-        </div>
+        <FormRequest
+          btnText={sectionData?.primaryButton}
+          privacyText={sectionData?.secondaryButton}
+        />
       </div>
-      <FormRequest btnText={sectionData?.primaryButton} card={card} />
+      <div className="relative h-[232px] w-[332px]">
+        <Image
+          src={sectionData?.image || "/assets/placeholder-gray.svg"}
+          alt="CTA Background Image"
+          fill
+          className="object-cover"
+          sizes="(max-width: 768px) 100vw, 50vw"
+        />
+      </div>
     </div>
   )
 }
