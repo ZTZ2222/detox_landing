5e770a82fca86518f7e33039ccb3d0d02eee091b
ZTZ <tzamirov@gmail.com>
2024-08-28 18:26:46 +0600

    update form metadata with locations + coordinates

--- src/app/[locale]/(protected)/admin/settings/form-meta.tsx
+++ src/app/[locale]/(protected)/admin/settings/form-meta.tsx
@@ -2,7 +2,7 @@
 
 import React from "react"
 import { zodResolver } from "@hookform/resolvers/zod"
-import { Save } from "lucide-react"
+import { PlusCircle, Save, Trash2 } from "lucide-react"
 import { useLocale, useTranslations } from "next-intl"
 import { useAction } from "next-safe-action/hooks"
 import { useForm } from "react-hook-form"
@@ -173,6 +173,123 @@ export default function MetadataForm({ metaData, className }: Props) {
                     )}
                   />
                 </div>
+
+                {/* Locations */}
+                <FormField
+                  control={form.control}
+                  name={`locations_${locale.id}` as keyof zMetaUpsert}
+                  render={({ field }) => {
+                    const locations = (field.value as string[]) || []
+
+                    const addLocation = () => {
+                      field.onChange([...locations, ""])
+                    }
+
+                    const removeLocation = (locationIndex: number) => {
+                      field.onChange(
+                        locations.filter((_, idx) => idx !== locationIndex),
+                      )
+                    }
+
+                    const updateLocation = (
+                      locationIndex: number,
+                      value: string,
+                    ) => {
+                      const updatedLocation = locations.map((location, idx) =>
+                        idx === locationIndex ? value : location,
+                      )
+                      field.onChange(updatedLocation)
+                    }
+
+                    return (
+                      <FormItem>
+                        <FormLabel>{t("label-locations")}</FormLabel>
+                        {locations.map((location, locationIndex) => (
+                          <div
+                            key={locationIndex}
+                            className="flex items-center space-x-2"
+                          >
+                            <span className="mr-2">{locationIndex + 1}</span>
+                            <FormControl>
+                              <Input
+                                value={location}
+                                onChange={e =>
+                                  updateLocation(locationIndex, e.target.value)
+                                }
+                                placeholder={t("placeholder-locations")}
+                              />
+                            </FormControl>
+                            <button
+                              type="button"
+                              onClick={() => removeLocation(locationIndex)}
+                              className="ml-2 text-red-500"
+                            >
+                              <Trash2 className="h-5 w-5" />
+                            </button>
+                          </div>
+                        ))}
+                        <button
+                          type="button"
+                          onClick={addLocation}
+                          className="flex items-center text-blue-500"
+                        >
+                          <PlusCircle className="mr-2 h-5 w-5" />
+                          {t("button-add-location")}
+                        </button>
+                      </FormItem>
+                    )
+                  }}
+                />
+
+                {/* Map Coordinates */}
+                <FormField
+                  control={form.control}
+                  name="map_coordinates"
+                  render={({ field }) => {
+                    const coordinates = field.value || []
+
+                    const updateCoordinate = (
+                      coordinateIndex: number,
+                      value: string,
+                    ) => {
+                      const updatedCoordinate = coordinates.map(
+                        (coordinate, idx) =>
+                          idx === coordinateIndex ? value : coordinate,
+                      )
+                      field.onChange(updatedCoordinate)
+                    }
+
+                    return (
+                      <FormItem>
+                        <FormLabel>{t("label-map-coordinates")}</FormLabel>
+                        {coordinates.map((coordinate, coordinateIndex) => (
+                          <div
+                            key={coordinateIndex}
+                            className="flex items-center space-x-2"
+                          >
+                            <span className="mr-2 w-[120px]">
+                              {coordinateIndex === 0
+                                ? t("latitude")
+                                : t("longitude")}
+                            </span>
+                            <FormControl>
+                              <Input
+                                value={coordinate}
+                                onChange={e =>
+                                  updateCoordinate(
+                                    coordinateIndex,
+                                    e.target.value,
+                                  )
+                                }
+                                placeholder={t("placeholder-coordinates")}
+                              />
+                            </FormControl>
+                          </div>
+                        ))}
+                      </FormItem>
+                    )
+                  }}
+                />
               </TabsContent>
             ))}
           </Tabs>
