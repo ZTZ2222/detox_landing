b00093baf34d340fd908e54f95a20f7b1681f772
ZTZ <tzamirov@gmail.com>
2024-08-30 01:03:23 +0600

    update three steps width bug

--- src/components/sections/three-steps.tsx
+++ src/components/sections/three-steps.tsx
@@ -17,21 +17,21 @@ export default async function ThreeSteps() {
             {sectionData?.subheading}
           </p>
         </div>
-        <div className="absolute -top-[68px] left-1/2 grid w-[167px] -translate-x-1/2 justify-items-center gap-3 text-center lg:-top-[100px] lg:w-[200px]">
-          <h3 className="font-semibold leading-5 text-text-primary lg:text-2xl">
+        <div className="absolute -top-[16px] left-1/2 grid -translate-x-1/2 justify-items-center gap-3 text-center lg:-top-[25px]">
+          <h3 className="absolute -top-16 w-48 text-sm font-semibold leading-5 text-text-primary lg:w-96 lg:text-2xl">
             {sectionData?.cards[0].title}
           </h3>
           <div className="size-8 rounded-full bg-[#B11515] lg:size-12" />
         </div>
-        <div className="absolute -bottom-[30px] -right-[60px] grid w-[153px] justify-items-center gap-3 text-center lg:-right-[150px] lg:bottom-[40px] lg:w-[296px]">
+        <div className="absolute -right-[22px] bottom-[55px] grid justify-items-center gap-3 text-center lg:-right-[20px] lg:bottom-[100px]">
           <div className="size-8 -translate-x-5 rounded-full bg-[#FFB919] lg:size-12" />
-          <h3 className="font-semibold leading-5 text-text-primary lg:text-2xl">
+          <h3 className="absolute -bottom-16 -right-[53px] w-48 text-sm font-semibold leading-5 text-text-primary lg:-left-[170px] lg:right-0 lg:w-96 lg:text-2xl">
             {sectionData?.cards[1].title}
           </h3>
         </div>
-        <div className="absolute -bottom-[30px] -left-[55px] grid w-[139px] justify-items-center gap-3 text-center lg:-left-[145px] lg:bottom-[40px] lg:w-[280px]">
+        <div className="absolute -left-[22px] bottom-[55px] grid justify-items-center gap-3 text-center lg:-left-[20px] lg:bottom-[100px]">
           <div className="size-8 translate-x-5 rounded-full bg-[#104D28] lg:size-12" />
-          <h3 className="font-semibold leading-5 text-text-primary lg:text-2xl">
+          <h3 className="absolute -bottom-16 -left-[53px] w-48 text-sm font-semibold leading-5 text-text-primary lg:-left-[170px] lg:w-96 lg:text-2xl">
             {sectionData?.cards[2].title}
           </h3>
         </div>
