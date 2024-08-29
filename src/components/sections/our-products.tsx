e718bf034a870552138371d693558c45862a0f4a
ZTZ <tzamirov@gmail.com>
2024-08-29 15:47:44 +0600

    update our products + product details with desktop adaptive

--- src/components/sections/our-products.tsx
+++ src/components/sections/our-products.tsx
@@ -1,9 +1,5 @@
 import Image from "next/image"
-import { Button } from "@/components/ui/button"
 import AnimatedButton from "@/components/shared/animated-button"
-import Heading from "@/components/shared/heading"
-import ServiceCard from "@/components/shared/service-card"
-import Subheading from "@/components/shared/subheading"
 import { getNormalizedSectionById } from "@/server/data-access-layer/content"
 
 export default async function OurProducts() {
@@ -13,76 +9,82 @@ export default async function OurProducts() {
   const product3 = sectionData?.cards[2]
 
   return (
-    <section id="our-products">
+    <section id="our-products" className="overflow-hidden">
       {/* Prodcut 1 */}
-      <div className="container grid justify-items-center gap-7 bg-[#B1151526]/[0.15] pb-6 pt-[120px]">
-        {/* Procut Image */}
-        <div className="relative h-[418.5px] w-[369px]">
-          <Image
-            src={product1?.image || "/assets/placeholder-gray.svg"}
-            alt="Product Image 1"
-            fill
-            className="object-contain"
-            sizes="(min-width: 1024px) 100vw, 50vw"
-          />
-        </div>
-        {/* Product Card */}
-        <div className="w-full max-w-md space-y-3 rounded-3xl bg-white/40 p-4">
-          <h3 className="text-lg font-semibold leading-6 text-text-secondary">
-            {product1?.title}
-          </h3>
-          <p className="text-2xl font-extrabold text-[#B11515]">
-            {product1?.description}
-          </p>
-          <AnimatedButton>{product1?.extra}</AnimatedButton>
+      <div className="relative bg-[#B1151526]/[0.15]">
+        <div className="container grid justify-items-center gap-7 pb-6 pt-[120px] lg:pb-[128px] lg:pt-[172px]">
+          {/* Procut Image */}
+          <div className="relative h-[418.5px] w-[271px] lg:h-[660px] lg:w-[423px]">
+            <Image
+              src={product1?.image || "/assets/placeholder-gray.svg"}
+              alt="Product Image 1"
+              fill
+              className="object-contain"
+              sizes="(min-width: 1024px) 100vw, 50vw"
+            />
+          </div>
+          {/* Product Card */}
+          <div className="w-full max-w-md space-y-3 rounded-3xl bg-white/40 p-4 lg:absolute lg:bottom-20 lg:right-20 lg:max-w-[294px] lg:bg-white xl:bg-white/40">
+            <h3 className="text-lg font-semibold leading-6 text-text-secondary lg:text-3xl">
+              {product1?.title}
+            </h3>
+            <p className="text-2xl font-extrabold text-[#B11515]">
+              {product1?.description}
+            </p>
+            <AnimatedButton>{product1?.extra}</AnimatedButton>
+          </div>
         </div>
       </div>
 
       {/* Prodcut 2 */}
-      <div className="container grid justify-items-center gap-7 bg-[#FFB91926]/[0.15] pb-6 pt-[120px]">
-        {/* Procut Image */}
-        <div className="relative h-[418.5px] w-[369px]">
-          <Image
-            src={product2?.image || "/assets/placeholder-gray.svg"}
-            alt="Product Image 1"
-            fill
-            className="object-contain"
-            sizes="(min-width: 1024px) 100vw, 50vw"
-          />
-        </div>
-        {/* Product Card */}
-        <div className="w-full max-w-md space-y-3 rounded-3xl bg-white/40 p-4">
-          <h3 className="text-lg font-semibold leading-6 text-text-secondary">
-            {product2?.title}
-          </h3>
-          <p className="text-2xl font-extrabold text-[#B11515]">
-            {product2?.description}
-          </p>
-          <AnimatedButton>{product2?.extra}</AnimatedButton>
+      <div className="relative bg-[#FFB91926]/[0.15]">
+        <div className="container grid justify-items-center gap-7 pb-6 pt-[120px] lg:pb-[111px] lg:pt-[145px]">
+          {/* Procut Image */}
+          <div className="relative h-[455px] w-[355px] lg:h-[705.5px] lg:w-[783px]">
+            <Image
+              src={product2?.image || "/assets/placeholder-gray.svg"}
+              alt="Product Image 1"
+              fill
+              className="object-contain"
+              sizes="(min-width: 1024px) 100vw, 50vw"
+            />
+          </div>
+          {/* Product Card */}
+          <div className="w-full max-w-md space-y-3 rounded-3xl bg-white/40 p-4 lg:absolute lg:bottom-20 lg:right-20 lg:max-w-[294px] lg:bg-white xl:bg-white/40">
+            <h3 className="text-lg font-semibold leading-6 text-text-secondary lg:text-3xl">
+              {product2?.title}
+            </h3>
+            <p className="text-2xl font-extrabold text-[#B11515]">
+              {product2?.description}
+            </p>
+            <AnimatedButton>{product2?.extra}</AnimatedButton>
+          </div>
         </div>
       </div>
 
       {/* Prodcut 3 */}
-      <div className="container grid justify-items-center gap-7 bg-[#104D2826]/[0.15] pb-6 pt-[120px]">
-        {/* Procut Image */}
-        <div className="relative h-[418.5px] w-[369px]">
-          <Image
-            src={product3?.image || "/assets/placeholder-gray.svg"}
-            alt="Product Image 1"
-            fill
-            className="object-contain"
-            sizes="(min-width: 1024px) 100vw, 50vw"
-          />
-        </div>
-        {/* Product Card */}
-        <div className="w-full max-w-md space-y-3 rounded-3xl bg-white/40 p-4">
-          <h3 className="text-lg font-semibold leading-6 text-text-secondary">
-            {product3?.title}
-          </h3>
-          <p className="text-2xl font-extrabold text-[#B11515]">
-            {product3?.description}
-          </p>
-          <AnimatedButton>{product3?.extra}</AnimatedButton>
+      <div className="relative bg-[#104D2826]/[0.15]">
+        <div className="container grid justify-items-center gap-7 pb-6 pt-[120px] lg:pb-[100px] lg:pt-[145px]">
+          {/* Procut Image */}
+          <div className="relative h-[427px] w-[375px] lg:h-[732px] lg:w-[763px]">
+            <Image
+              src={product3?.image || "/assets/placeholder-gray.svg"}
+              alt="Product Image 1"
+              fill
+              className="object-contain"
+              sizes="(min-width: 1024px) 100vw, 50vw"
+            />
+          </div>
+          {/* Product Card */}
+          <div className="w-full max-w-md space-y-3 rounded-3xl bg-white/40 p-4 lg:absolute lg:bottom-20 lg:right-20 lg:max-w-[294px] lg:bg-white xl:bg-white/40">
+            <h3 className="text-lg font-semibold leading-6 text-text-secondary lg:text-3xl">
+              {product3?.title}
+            </h3>
+            <p className="text-2xl font-extrabold text-[#B11515]">
+              {product3?.description}
+            </p>
+            <AnimatedButton>{product3?.extra}</AnimatedButton>
+          </div>
         </div>
       </div>
     </section>
--- src/components/sections/product-details.tsx
+++ src/components/sections/product-details.tsx
@@ -6,26 +6,28 @@ import { getNormalizedSectionById } from "@/server/data-access-layer/content"
 export default async function ProductDetails() {
   const sectionData = await getNormalizedSectionById("product-details")
   return (
-    <section className="container relative grid h-screen items-center justify-items-center">
-      <div className="relative z-10 grid max-w-lg justify-items-center gap-5 rounded-3xl bg-white/60 px-6 py-8 text-center backdrop-blur-md">
-        <h4 className="w-full rounded-md border border-text-secondary p-3 text-center font-bold leading-5 text-text-primary">
+    <section className="relative grid h-screen place-content-center lg:h-screen">
+      <div className="container relative z-10 grid max-w-lg justify-items-center gap-5 rounded-3xl bg-white/60 px-6 py-8 text-center backdrop-blur-md lg:max-w-4xl lg:px-36 lg:py-12 xl:max-w-5xl xl:px-[220px]">
+        {/* Blocks */}
+        <h4 className="w-full rounded-md border border-text-secondary p-3 text-center font-bold leading-5 text-text-primary lg:text-xl lg:leading-6">
           {sectionData?.cards[0].title}
         </h4>
         <span className="text-5xl font-medium text-text-primary">+</span>
-        <h4 className="w-full rounded-md border border-text-secondary p-3 text-center font-bold leading-5 text-text-primary">
+        <h4 className="w-full rounded-md border border-text-secondary p-3 text-center font-bold leading-5 text-text-primary lg:text-xl lg:leading-6">
           {sectionData?.cards[1].title}
         </h4>
-        <h4 className="w-full rounded-md border border-text-secondary p-3 text-center font-bold leading-5 text-text-primary">
+        <h4 className="w-full rounded-md border border-text-secondary p-3 text-center font-bold leading-5 text-text-primary lg:text-xl lg:leading-6">
           {sectionData?.cards[2].title}
         </h4>
-        <p className="font-semibold leading-5 text-text-secondary">
+        {/* Texts */}
+        <p className="font-semibold leading-5 text-text-secondary lg:text-xl lg:leading-6">
           {sectionData?.sectionName}
         </p>
         <Separator className="bg-text-primary" />
-        <h3 className="font-bold leading-5 text-text-secondary">
+        <h3 className="font-bold leading-5 text-text-secondary lg:text-xl lg:leading-6">
           {sectionData?.heading}
         </h3>
-        <p className="leading-5 text-text-secondary">
+        <p className="leading-5 text-text-secondary lg:text-xl lg:leading-6">
           {sectionData?.subheading}
         </p>
       </div>
