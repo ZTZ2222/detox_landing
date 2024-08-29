54dcaf254adf32e2399b4c79fa12a09895174d73
ZTZ <tzamirov@gmail.com>
2024-08-29 15:48:14 +0600

    update product features + three steps with desktop adaptive

--- src/components/sections/product-features.tsx
+++ src/components/sections/product-features.tsx
@@ -15,13 +15,15 @@ export default async function ProductFeatures() {
   return (
     <section
       id="product-features"
-      className="container min-h-screen space-y-4 py-20"
+      className="container min-h-screen space-y-4 py-20 md:min-h-fit lg:py-32"
     >
       <div className="space-y-3">
-        <Heading className="text-2xl leading-6">{sectionData?.heading}</Heading>
+        <Heading className="text-2xl leading-6 lg:text-3xl lg:leading-7 xl:text-4xl xl:leading-10">
+          {sectionData?.heading}
+        </Heading>
         <Subheading>{sectionData?.subheading}</Subheading>
       </div>
-      <div className="grid gap-5">
+      <div className="grid gap-5 md:grid-cols-2 lg:grid-cols-3">
         {sectionData?.cards.map((card, index) => (
           <Card
             key={index}
@@ -32,16 +34,16 @@ export default async function ProductFeatures() {
               style={{
                 background: `${card.extra}`,
               }}
-              className="flex h-[72px] items-start justify-center p-3"
+              className="flex min-h-[72px] items-start justify-center p-3 lg:min-h-24 lg:px-6 lg:py-4"
             >
-              <CardTitle className="text-xl font-bold leading-6 text-white">
+              <CardTitle className="text-xl font-bold leading-6 text-white lg:text-2xl">
                 {card.title}
               </CardTitle>
-              <CardDescription className="sr-only border-yellow-200">
+              <CardDescription className="sr-only">
                 {card.description}
               </CardDescription>
             </CardHeader>
-            <CardContent className="space-y-3 p-3 text-text-secondary">
+            <CardContent className="space-y-3 p-3 text-text-secondary lg:px-6 lg:pb-6 lg:text-xl lg:leading-6">
               <p>{card.description}</p>
               <p>{card.bullets.join(" / ")}</p>
             </CardContent>
--- src/components/sections/three-steps.tsx
+++ src/components/sections/three-steps.tsx
@@ -8,30 +8,30 @@ export default async function ThreeSteps() {
       id="three-steps"
       className="container grid place-content-center pb-[30px] pt-[70px]"
     >
-      <div className="relative grid size-[255px] place-content-center rounded-full border border-dashed border-black">
-        <div className="grid size-[173px] place-content-center space-y-2 rounded-full bg-white px-4 py-10 text-center">
-          <h2 className="text-text-primary text-2xl font-bold">
+      <div className="relative grid size-[255px] place-content-center rounded-full border border-dashed border-black lg:size-[452px]">
+        <div className="grid size-[173px] place-content-center space-y-2 rounded-full bg-white px-4 py-10 text-center lg:size-[292px]">
+          <h2 className="text-2xl font-bold text-text-primary lg:text-4xl">
             {sectionData?.heading}
           </h2>
-          <p className="text-text-secondary font-medium leading-6">
+          <p className="font-medium leading-6 text-text-secondary lg:text-xl">
             {sectionData?.subheading}
           </p>
         </div>
-        <div className="absolute -top-[68px] left-1/2 grid w-[167px] -translate-x-1/2 justify-items-center gap-3 text-center">
-          <h3 className="text-text-primary font-semibold leading-5">
+        <div className="absolute -top-[68px] left-1/2 grid w-[167px] -translate-x-1/2 justify-items-center gap-3 text-center lg:-top-[100px] lg:w-[200px]">
+          <h3 className="font-semibold leading-5 text-text-primary lg:text-2xl">
             {sectionData?.cards[0].title}
           </h3>
-          <div className="size-8 rounded-full bg-[#B11515]" />
+          <div className="size-8 rounded-full bg-[#B11515] lg:size-12" />
         </div>
-        <div className="absolute -bottom-[30px] -right-[60px] grid w-[153px] justify-items-center gap-3 text-center">
-          <div className="size-8 -translate-x-5 rounded-full bg-[#FFB919]" />
-          <h3 className="text-text-primary font-semibold leading-5">
+        <div className="absolute -bottom-[30px] -right-[60px] grid w-[153px] justify-items-center gap-3 text-center lg:-right-[150px] lg:bottom-[40px] lg:w-[296px]">
+          <div className="size-8 -translate-x-5 rounded-full bg-[#FFB919] lg:size-12" />
+          <h3 className="font-semibold leading-5 text-text-primary lg:text-2xl">
             {sectionData?.cards[1].title}
           </h3>
         </div>
-        <div className="absolute -bottom-[30px] -left-[55px] grid w-[139px] justify-items-center gap-3 text-center">
-          <div className="size-8 translate-x-5 rounded-full bg-[#104D28]" />
-          <h3 className="text-text-primary font-semibold leading-5">
+        <div className="absolute -bottom-[30px] -left-[55px] grid w-[139px] justify-items-center gap-3 text-center lg:-left-[145px] lg:bottom-[40px] lg:w-[280px]">
+          <div className="size-8 translate-x-5 rounded-full bg-[#104D28] lg:size-12" />
+          <h3 className="font-semibold leading-5 text-text-primary lg:text-2xl">
             {sectionData?.cards[2].title}
           </h3>
         </div>
